f8813afd2aeb2cd59340e4c9620c5728594e0fd1
Andy Armstrong <andy@hexten.net>
2007-05-24 03:13:25 +0000

    Tagged 1.0

--- lib/CGI/Simple/Cookie.pm
+++ lib/CGI/Simple/Cookie.pm
@@ -12,7 +12,7 @@ package CGI::Simple::Cookie;
 
 use strict;
 use vars '$VERSION';
-$VERSION = '0.83';
+$VERSION = '1.0';
 use CGI::Simple::Util qw(rearrange unescape escape);
 use overload '""' => \&as_string, 'cmp' => \&compare, 'fallback' => 1;
 
