364c7f26d9ff9d13212b0121791c01cd0a1cd7e0
Andy Armstrong <andy@hexten.net>
2008-05-16 14:37:41 +0000

    Tagging 1.105, version bump

--- lib/CGI/Simple/Util.pm
+++ lib/CGI/Simple/Util.pm
@@ -1,7 +1,7 @@
 package CGI::Simple::Util;
 use strict;
 use vars qw( $VERSION @EXPORT_OK @ISA $UTIL );
-$VERSION = '1.105';
+$VERSION = '1.106';
 require Exporter;
 @ISA       = qw( Exporter );
 @EXPORT_OK = qw(
