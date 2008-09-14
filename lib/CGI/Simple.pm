838dbe7b2251985d2eb14072b959934b2a2d8afc
Andy Armstrong <andy@hexten.net>
2008-09-14 13:29:45 +0000

    Tagging 1.106, version bump

--- lib/CGI/Simple.pm
+++ lib/CGI/Simple.pm
@@ -14,7 +14,7 @@ use vars qw(
  $NPH $DEBUG $NO_NULL $FATAL *in
 );
 
-$VERSION = "1.106";
+$VERSION = "1.107";
 
 # you can hard code the global variable settings here if you want.
 # warning - do not delete the unless defined $VAR part unless you
@@ -1439,7 +1439,7 @@ CGI::Simple - A Simple totally OO CGI interface that is CGI.pm compliant
 
 =head1 VERSION
 
-This document describes CGI::Simple version 1.106.
+This document describes CGI::Simple version 1.107.
 
 =head1 SYNOPSIS
 
--- lib/CGI/Simple/Util.pm
+++ lib/CGI/Simple/Util.pm
@@ -1,7 +1,7 @@
 package CGI::Simple::Util;
 use strict;
 use vars qw( $VERSION @EXPORT_OK @ISA $UTIL );
-$VERSION = '1.106';
+$VERSION = '1.107';
 require Exporter;
 @ISA       = qw( Exporter );
 @EXPORT_OK = qw(
