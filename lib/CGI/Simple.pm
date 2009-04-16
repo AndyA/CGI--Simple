13d75e804261c2015905702c80ee4d49ae9f87ce
Andy Armstrong <andy@hexten.net>
2009-04-16 17:57:25 +0000

    Bump version to 1.110

--- lib/CGI/Simple.pm
+++ lib/CGI/Simple.pm
@@ -13,7 +13,7 @@ use vars qw(
  $NPH $DEBUG $NO_NULL $FATAL *in
 );
 
-$VERSION = "1.109";
+$VERSION = "1.110";
 
 # you can hard code the global variable settings here if you want.
 # warning - do not delete the unless defined $VAR part unless you
@@ -1445,7 +1445,7 @@ CGI::Simple - A Simple totally OO CGI interface that is CGI.pm compliant
 
 =head1 VERSION
 
-This document describes CGI::Simple version 1.109.
+This document describes CGI::Simple version 1.110.
 
 =head1 SYNOPSIS
 
--- lib/CGI/Simple/Cookie.pm
+++ lib/CGI/Simple/Cookie.pm
@@ -12,7 +12,7 @@ package CGI::Simple::Cookie;
 
 use strict;
 use vars '$VERSION';
-$VERSION = '1.109';
+$VERSION = '1.110';
 use CGI::Simple::Util qw(rearrange unescape escape);
 use overload '""' => \&as_string, 'cmp' => \&compare, 'fallback' => 1;
 
