9da94494ba0e503f3e5c9249e33c8a8637670cd4
Mark Stosberg <mark@stosberg.com>
2009-08-19 21:15:10 -0400

    Optimize Vars(): Don't build %hash if we aren't going to use it.

--- lib/CGI/Simple.pm
+++ lib/CGI/Simple.pm
@@ -683,11 +683,11 @@ sub keywords {
 sub Vars {
   my $self = shift;
   $self->{'.sep'} = shift || $self->{'.sep'} || "\0";
-  my %hash;
-  for my $param ( $self->param ) {
-    $hash{$param} = join $self->{'.sep'}, $self->param( $param );
-  }
   if (wantarray) {
+      my %hash;
+      for my $param ( $self->param ) {
+          $hash{$param} = join $self->{'.sep'}, $self->param( $param );
+      }
       return %hash;
   }
   else {
