0a224be7c2bff60c19e62beeeb0630475bbeef52
Gabor Szabo <gabor@szabgab.com>
2014-10-19 16:39:57 +0300

    set version numbers to 1.12 to be the same in every module

--- lib/CGI/Simple.pm
+++ lib/CGI/Simple.pm
@@ -13,7 +13,7 @@ use vars qw(
  $NPH $DEBUG $NO_NULL $FATAL *in
 );
 
-$VERSION = "1.115";
+$VERSION = "1.12";
 
 # you can hard code the global variable settings here if you want.
 # warning - do not delete the unless defined $VAR part unless you
--- lib/CGI/Simple/Util.pm
+++ lib/CGI/Simple/Util.pm
@@ -1,7 +1,7 @@
 package CGI::Simple::Util;
 use strict;
 use vars qw( $VERSION @EXPORT_OK @ISA $UTIL );
-$VERSION = '1.114';
+$VERSION = '1.12';
 require Exporter;
 @ISA       = qw( Exporter );
 @EXPORT_OK = qw(
