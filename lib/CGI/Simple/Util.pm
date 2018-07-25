9c90ccefb6f871c86a68888055c8d56d7802ed75
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-07-25 16:17:01 +0100

    - Made t/manifest.t AUTHOR only (RT #125383). - Removed +x bits from test scripts.

--- lib/CGI/Simple/Util.pm
+++ lib/CGI/Simple/Util.pm
@@ -2,7 +2,7 @@ package CGI::Simple::Util;
 use strict;
 use warnings;
 use vars qw( $VERSION @EXPORT_OK @ISA $UTIL );
-$VERSION = '1.15';
+$VERSION = '1.16';
 require Exporter;
 @ISA       = qw( Exporter );
 @EXPORT_OK = qw(
