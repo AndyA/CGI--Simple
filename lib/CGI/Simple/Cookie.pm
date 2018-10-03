2987472a2d7bae917942b948ed4f4cf357cc9259
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-10-03 15:21:16 +0100

    - Merged pull request #2 from jjatria/samesite, adding   SameSite support to Cookie handling.

--- lib/CGI/Simple/Cookie.pm
+++ lib/CGI/Simple/Cookie.pm
@@ -13,7 +13,7 @@ package CGI::Simple::Cookie;
 use strict;
 use warnings;
 use vars '$VERSION';
-$VERSION = '1.17';
+$VERSION = '1.18';
 use CGI::Simple::Util qw(rearrange unescape escape);
 use overload '""' => \&as_string, 'cmp' => \&compare, 'fallback' => 1;
 
