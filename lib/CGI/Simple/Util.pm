b6171f255a7a279fa69e09768bdf213bc05b1f4f
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2022-01-06 15:59:29 +0000

    - Removed unneeded Build.PL #11, thanks @haarg. - Moved prereqs to correct phase #12, thanks @haarg.

--- lib/CGI/Simple/Util.pm
+++ lib/CGI/Simple/Util.pm
@@ -2,7 +2,7 @@ package CGI::Simple::Util;
 use strict;
 use warnings;
 use vars qw( $VERSION @EXPORT_OK @ISA $UTIL );
-$VERSION = '1.26';
+$VERSION = '1.27';
 require Exporter;
 @ISA       = qw( Exporter );
 @EXPORT_OK = qw(
